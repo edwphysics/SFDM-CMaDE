10b6f2d397ede1c1d0340afbaf4d6e2bba4e7949
Edwin Perez <edwpes8a@gmail.com>
2024-05-09 09:48:48 -0600

    new file: Solution of the Alma-Ureña (2016) paper equations
    	- The system is converging rapidly when m increases giving the right energy densities
    	- The solver does not require a huge amount of points to converge
    	- The cut-off is still not implemented
    
    	Background

--- Background-Angular/SFDM+CMaDE.py
+++ Background-Angular/SFDM+CMaDE.py
@@ -1,8 +1,8 @@
 '''
 
-Compton Mass Dark Energy & Scalar Field Dark Matter.
+Scalar Field Dark Matter with the Alma-Ureña (2016) system of equations.
 
-Combination of the SFDM and CMaDE model equations. 
+Combination of the SFDM model equations. 
 Here, the density parameters of the components of the universe are calculated solving the dynamical equations using the ABMM4 method.
 
 Modified from the original code by Luis Osvaldo Tellez Tovar for the paper "The quantum character of the Scalar Field Dark Matter" by Tonatiuh Matos
@@ -10,8 +10,6 @@ Modified from the original code by Luis Osvaldo Tellez Tovar for the paper "The 
 '''
 
 import numpy as np
-from scipy.integrate import odeint
-from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 import math
 import time
@@ -24,13 +22,13 @@ class DarkM:
         self.s    = self.mass/self.H0
 
         # Scale factor range
-        self.NP = 10000000
+        self.NP = 1000000
         self.Ni = np.log(1.e-0)
         self.Nf = np.log(1.e-6)
         self.d  = (self.Nf - self.Ni)/ self.NP
         self.t  = [self.Ni + self.d* i for i in np.arange(self.NP)]
 
-    # Runge-Kutta 4 initial0ices the method ABM4
+    # Runge-Kutta 4 initialices the method ABM4
     def rk4(self, func, y_0, t):
         y = np.zeros([4, len(y_0)])
         y[0] = y_0
@@ -63,7 +61,7 @@ class DarkM:
 
             # Prints N, F, and Omega SFDM
             if i % 50000 == 0:
-                print("{:<10}\t{:<10}\t{:<10}".format(t[i], np.sum(np.square(np.array(y[i,:-1]))), np.sum(np.square(np.array(y[i,:2])))))
+                print("{:<10}\t{:<10}\t{:<10}".format(t[i], y[i,1] + np.sum(np.square(np.array(y[i,2:-1]))), y[i,1]))
 
             h   = self.d
             k_4 = k_3
@@ -80,13 +78,13 @@ class DarkM:
 
     # Initial conditions from today comsological observations
     def solver(self):
-        y0 = np.array([np.sqrt(0.22994),  # x: x0 Kinetic energy - small to avoid 1/x divergence
-                       np.sqrt(0.),       # u: x2 Potential energy
-                       np.sqrt(0.00004),  # z: x4 Radiation
-                       np.sqrt(0.00002),  # n: x5 Neutrinos
-                       np.sqrt(0.04),     # b: x6 Baryons
-                       np.sqrt(0.73),     # l: x7 Lambda
-                       1.e4])             # s: x8 Spurious Variable
+        y0 = np.array([np.sqrt(0.),       # x1: Th Theta
+                       np.sqrt(0.22994),  # x2: Om Omega_Phi
+                       np.sqrt(0.00004),  # x3:  z Radiation
+                       np.sqrt(0.00002),  # x4: nu Neutrinos
+                       np.sqrt(0.04),     # x5:  b Baryons
+                       np.sqrt(0.73),     # x6:  l Lambda
+                       1.e9])             # x7:  y1 Spurious Variable
 
         # Solve the SoE with the ABM4 or RK4 algorithms
         y_result = self.ABM4(self.RHS, y0, self.t)
@@ -96,26 +94,26 @@ class DarkM:
 
     # System of Equations
     def RHS(self, t, y):
-        x0, x2, x4, x5, x6, x7, x8 = y
+        x1, x2, x3, x4, x5, x6, x7 = y
 
         CTer = 4./3.
         kc   = 1.
         Q    = 1.
-        Pe   = 2.* x0**2 + 4.* x4**2/3. + 4.* x5**2/3. + x6**2 + (kc - 1.)* (Q/np.pi)* np.sqrt(2/3.)* x7**3* np.exp(-t)
+        Pe   = 2.* x2* np.sin(x1/2.)**2 + CTer* x3**2 + CTer* x4**2 + x5**2
 
-        return np.array([-3.* x0 - x8* x2 + 1.5* x0* Pe - (Q* kc/np.pi)* np.sqrt(3/2.)* (x7**3/x0)* np.exp(-t),
-                         x0* x8 + 1.5* x2* Pe,
+        return np.array([-3* np.sin(x1) + x7,
+                         3* (Pe - 1. + np.cos(x1))* x2,
+                         1.5* x3* (Pe - CTer),
                          1.5* x4* (Pe - CTer),
-                         1.5* x5* (Pe - CTer),
-                         1.5* x6* (Pe - 1.),
-                         1.5* x7* Pe + (Q/np.pi)* np.sqrt(3/2.)* x7**2* np.exp(-t),
-                         #-1.5* x8**(-2)])
-                         1.5* Pe* x8])
+                         1.5* x5* (Pe - 1.),
+                         1.5* x6* Pe,
+                         #-1.5* x7**(-2)])
+                         1.5* Pe* x7])
 
     # Plotting Function
     def plot(self):
         #En este arreglo se guardan los resultados de la funcion solver. Las variables se acomodan como en la funcion RHS.
-        z0, z2, z4, z5, z6, z7, z8 = self.solver().T
+        z1, z2, z3, z4, z5, z6, z7 = self.solver().T
         #x, u, z, nu, l, s, b = self.solver().T
 
         fig3 = plt.figure(figsize=(9,10))
@@ -131,23 +129,23 @@ class DarkM:
         ax9  = fig9.add_subplot(111)       #La grafica correspondiente a la nueva ventana.
 
         i = 0
-        tiempo = w0 = w2 = w4 = w5 = w6 = w7 = w8 = np.array([])
+        tiempo = w1 = w2 = w3 = w4 = w5 = w6 = w7 = np.array([])
 
-        for t, aux0, aux2, aux4, aux5, aux6, aux7, aux8  in zip(self.t, z0, z2, z4, z5, z6, z7, z8):
+        for t, aux1, aux2, aux3, aux4, aux5, aux6, aux7  in zip(self.t, z1, z2, z3, z4, z5, z6, z7):
             #Resolucion de las graficas
             if i % 200 == 0:
                tiempo = np.append(tiempo, np.exp(t))  # Scale factor from e-folding N
-               w0 = np.append(w0, aux0)
+               w1 = np.append(w1, aux1)
                w2 = np.append(w2, aux2)
+               w3 = np.append(w3, aux3)
                w4 = np.append(w4, aux4)
                w5 = np.append(w5, aux5)
                w6 = np.append(w6, aux6)
                w7 = np.append(w7, aux7)
-               w8 = np.append(w8, aux8)
             i += 1
 
         # Background Scalar Field
-        ax2.semilogx(tiempo, np.sqrt(6.)* w2/w8, 'black', label=r"$\kappa\Phi_0$")
+        ax2.semilogx(tiempo, -2* np.sqrt(6.)* np.sqrt(w2)* np.cos(w1/2.)/w7, 'black', label=r"$\kappa\Phi_0$")
         ax2.set_ylabel(r'$\kappa\Phi_0(a)$', fontsize=20)
         ax2.set_xlabel(r'$a$', fontsize=15)
         ax2.legend(loc = 'best', fontsize = 'xx-large')
@@ -155,11 +153,11 @@ class DarkM:
         #plt.show()
 
         #Dark Matter
-        ax3.semilogx(tiempo, w0**2 + w2**2, 'black', label=r"$\Omega_{SFDM}$")
-        ax3.semilogx(tiempo, w4**2, 'blue', label=r"$\Omega_{\gamma}$") #radiacion
-        ax3.semilogx(tiempo, w5**2, 'orange', label=r"$\Omega_{v}$") #neutrinos
-        ax3.semilogx(tiempo, w6**2, 'red', label=r"$\Omega_b$")  #bariones
-        ax3.semilogx(tiempo, w7**2, 'green', label=r"$\Omega_{\Lambda}$")  #constante cosmologica
+        ax3.semilogx(tiempo, w2, 'black', label=r"$\Omega_{SFDM}$")
+        ax3.semilogx(tiempo, w3**2, 'blue', label=r"$\Omega_{\gamma}$")     #radiation
+        ax3.semilogx(tiempo, w4**2, 'orange', label=r"$\Omega_{v}$")        #neutrinos
+        ax3.semilogx(tiempo, w5**2, 'red', label=r"$\Omega_b$")             #baryons
+        ax3.semilogx(tiempo, w6**2, 'green', label=r"$\Omega_{\Lambda}$")   #Lambda
         ax3.set_ylabel(r'$\Omega(a)$', fontsize=20) #original
         ax3.set_xlabel(r'$a$', fontsize=15)
         ax3.legend(loc = 'best', fontsize = 'xx-large')
@@ -167,7 +165,7 @@ class DarkM:
         #plt.show()        
 
         # Friedmann Restriction
-        ax9.semilogx(tiempo, w0**2 + w2**2 + w4**2 + w5**2 + w6**2 + w7**2, 'black', label=r"$F$")
+        ax9.semilogx(tiempo, w2 + w3**2 + w4**2 + w5**2 + w6**2, 'black', label=r"$F$")
         ax9.set_ylabel(r'$F(a)$', fontsize=20)
         ax9.set_xlabel(r'$a$', fontsize=15)
         ax9.legend(loc = 'best', fontsize = 'xx-large')
@@ -175,7 +173,7 @@ class DarkM:
         #plt.show()
 
         # Spurious Variable
-        ax8.semilogx(tiempo, w8, 'black', label=r"$s$")
+        ax8.semilogx(tiempo, w7, 'black', label=r"$s$")
         ax8.set_ylabel(r'$s$', fontsize=20)
         ax8.set_xlabel(r'$a$', fontsize=15)
         ax8.legend(loc = 'best', fontsize = 'xx-large')
